3c6d2978924676d3ba33acae4c08c5daa745b7b0
piamanzon <49318818+piamanzon@users.noreply.github.com>
2021-04-27 08:39:21 -0600

    Final Version YYC

---
+++ FlightData/YYC_Excel.py
@@ -0,0 +1,196 @@
+'''
+Created on Oct 5, 2020
+
+@author: pia.manzon
+'''
+from openpyxl import workbook #pip install openpyxl
+from openpyxl import load_workbook
+
+from openpyxl.styles import Border, Side, Alignment, Font, NamedStyle
+from datetime import datetime
+from datetime import date
+
+
+class Excel():
+    def __init__(self,excelPath):
+        self.filePath = excelPath
+        self.workbook = load_workbook(excelPath)
+        self.worksheet = self.workbook.active
+        self.myThinBorder = Border(top=Side(border_style='thin', color='000000'))
+        self.addStyle()
+       
+    def addStyle(self):
+        if(not self.styleExists("RawDataStyle")):
+            self.rawdataStyle = NamedStyle(name = "RawDataStyle", font = Font(name = 'Arial', size =10), alignment = Alignment(horizontal = 'left', vertical = 'bottom'))
+            self.workbook.add_named_style(self.rawdataStyle)
+        if(not self.styleExists("ChartDataStyle")):
+            self.chartDataStyle = NamedStyle(name = "ChartDataStyle", font = Font(name = 'Arial', size =10), alignment = Alignment(horizontal = 'center', vertical = 'bottom'))
+            self.workbook.add_named_style(self.chartDataStyle)
+        if(not self.styleExists("CellStyle")):
+            self.cellStyle = NamedStyle(name = "CellStyle", number_format = 'd-mmm-yy', font = Font(name = 'Arial', size =10), alignment = Alignment(horizontal = 'left', vertical = 'bottom'))
+            self.workbook.add_named_style(self.cellStyle)
+      
+    def styleExists(self,styleName):
+        result = self.workbook.named_styles
+         
+        if (styleName in result):
+            return True
+        return False
+        
+    def getMaxRow(self,columnName):
+        maxRow = max((c.row for c in self.worksheet[columnName] if c.value is not None))
+        return maxRow
+    
+    def setBorder(self,maxRow,columnStart):
+        for index in range(columnStart,columnStart+3):
+            self.worksheet.cell(row = maxRow, column = index).border = self.myThinBorder 
+        
+    def inputNewData (self,maxRow,flightDataList,city):       
+        newMaxRow = maxRow + 1
+        columnStart = 0
+        borderRow = newMaxRow
+
+        if (city == "Vancouver"):
+            columnStart = 3
+                 
+        else:
+            columnStart = 7
+              
+#Update Flight Info
+#===============================================================================
+        for index in range(len(flightDataList)):
+            tempCell =  self.worksheet.cell(row=newMaxRow, column = columnStart)
+            tempCell.value = datetime.strptime(flightDataList[index]['Date'], '%m/%d/%Y')
+            tempCell.style = "CellStyle"
+            tempCell =  self.worksheet.cell(row=newMaxRow, column = (columnStart+1))
+            tempCell.value = flightDataList[index]['Flight']
+            tempCell.style = "RawDataStyle"
+            tempCell = self.worksheet.cell(row=newMaxRow, column = (columnStart+2))
+            tempCell.value = flightDataList[index]['From']
+            tempCell.style = "RawDataStyle"
+            newMaxRow+=1   
+        self.setBorder(borderRow, columnStart)
+
+#Update Daily Summary
+#===============================================================================  
+    def updateDailySummary(self,flightVancouverList,flightTorontoList):
+        dateToday = datetime.strptime(flightVancouverList[0]['Date'], '%m/%d/%Y')
+        columnStart = 12 
+        newMaxRow = self.getMaxRow('l') + 1
+        tempCell = self.worksheet.cell(row=newMaxRow, column = columnStart)
+        tempCell.value = dateToday
+        tempCell.style = "CellStyle"
+        rowC = 'C'+str(self.getMaxRow('C'))
+        rowL = 'L' + str(newMaxRow)
+        vanCell = 'M' + str(newMaxRow)
+        torCell = 'N' + str(newMaxRow)
+        self.worksheet[vanCell] = "=COUNTIF(C13:" + rowC + "," + rowL + ")"
+        self.worksheet[vanCell].style = "ChartDataStyle"
+        rowC = 'G'+str(self.getMaxRow('G'))
+        self.worksheet[torCell] = "=COUNTIF(G13:" + rowC + "," + rowL + ")"
+        self.worksheet[torCell].style = "ChartDataStyle"
+        
+        
+   
+    def updateWeeklySummary(self):
+        x = 133
+        while x <=(137):
+            dateMaxRow = self.getMaxRow('L') #Max weekly date row
+            dateMaxRow = x
+           
+            dateToday = self.worksheet.cell(row=dateMaxRow, column = 12).value
+            dateToday = dateToday[:7] + "20" + dateToday[7:]
+                   
+            dateToday= datetime.strptime(dateToday, "%d-%b-%Y")
+            print("Date today is ", dateToday)
+            currentWeek = dateToday.isocalendar()[1]
+            print("Current week is ", currentWeek)
+            
+            tempCellVal = self.worksheet.cell(row=dateMaxRow-1, column = 12).value
+            print(tempCellVal)
+            formattedDate = tempCellVal[:7] + "20" + tempCellVal[7:]
+            dateLastWeek = datetime.strptime(formattedDate, "%d-%b-%Y")
+            print("Date last week is ", tempCellVal)
+            
+            weekBefore = dateLastWeek.isocalendar()[1]
+            print("Week before is ", weekBefore)
+            
+            flightMaxRow = self.getMaxRow('M') 
+            flightMaxRow = x
+            tempCell = self.worksheet.cell(row=flightMaxRow, column = 13)
+            tempCellYYZ = self.worksheet.cell(row=flightMaxRow, column = 14)
+            dataList = []
+            dataListYYZ = []
+            average = 0
+            averageYYZ = 0
+            index = 0
+            if(currentWeek != weekBefore):
+                average = tempCell.value
+                averageYYZ = tempCellYYZ.value
+                newMaxRow = self.getMaxRow('O')+1
+                tempCell = self.worksheet.cell(row=newMaxRow, column = 15)
+                
+                tempCell.value= self.worksheet.cell(row=x, column = 12).value
+                tempCell.style = "RawDataStyle"
+             
+                tempCell = self.worksheet.cell(row=newMaxRow, column = 16)
+                tempCellYYZ = self.worksheet.cell(row=newMaxRow, column = 17)
+                tempCell.value = average
+                tempCellYYZ.value = averageYYZ
+                tempCell.style = "ChartDataStyle"
+                tempCellYYZ.style = "ChartDataStyle"
+
+            else:
+                index = flightMaxRow
+                
+                while (currentWeek == weekBefore):
+                    tempCell = self.worksheet.cell(row=index, column = 13)
+                    tempCellYYZ = self.worksheet.cell(row=index, column = 14)
+                    print("Current date flight is ", tempCell.value)
+                    dataList.append(tempCell.value)
+                    dataListYYZ.append(tempCellYYZ.value)
+                    index= index -1
+                    currentWeek = weekBefore
+                    print("Current week is ", currentWeek)
+                    tempCellVal = self.worksheet.cell(row=index-1, column = 12).value
+                    dateLastWeek = tempCellVal
+                    
+                    print("Date last week is ", dateLastWeek)
+                    weekBefore = dateLastWeek.isocalendar()[1] 
+                    print("Week before is ", weekBefore)
+                    
+                tempCell = self.worksheet.cell(row=index, column = 13)
+                print("Current date flight is ", tempCell.value)
+                dataList.append(tempCell.value)
+                average = round((sum(dataList)/len(dataList)),0) 
+                dataListYYZ.append(tempCellYYZ.value)
+                averageYYZ = round((sum(dataListYYZ)/len(dataListYYZ)),0)
+                print ("Average is ", average)
+                newMaxRow = self.getMaxRow('O')
+                tempCell = self.worksheet.cell(row=newMaxRow, column = 15)
+                
+                tempCell.value= self.worksheet.cell(row=index, column = 12).value
+              
+                tempCell = self.worksheet.cell(row=newMaxRow, column = 16)
+                tempCell.value = average
+                tempCell.style = "ChartDataStyle"
+                tempCellYYZ = self.worksheet.cell(row=newMaxRow, column = 17)
+                tempCellYYZ.value = averageYYZ
+                tempCellYYZ.style = "ChartDataStyle"
+            x = x + 1
+     
+            
+        
+           
+    def isAlreadyUpdated(self,dateToday):
+        maxRow = self.getMaxRow('C')
+        lastDateUpdate = self.worksheet.cell(row = maxRow, column = 3).internal_value.strftime('%m/%d/%Y')
+       
+        if (dateToday == lastDateUpdate):
+            return True
+        return False
+              
+    def saveFile(self):
+        self.workbook.save(self.filePath)
+        
+        
---
+++ FlightData/YYC_Webpage.py
@@ -0,0 +1,66 @@
+'''
+Created on Oct 5, 2020
+
+@author: pia.manzon
+
+'''
+#Selenium Imports
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+#Date and Time Imports
+from datetime import date
+import time
+
+
+#Selenium
+chromedriver_path = r'C:\Users\pia.manzon\Downloads\chromedriver_win32/chromedriver.exe'
+driver = webdriver.Chrome(executable_path=chromedriver_path) # This will open the Chrome window
+yycFlightURL = 'https://www.yyc.com/en-us/travellerinfo/flightinformation/flightschedule.aspx'
+driver.get(yycFlightURL) #open website
+
+   
+def clickArrivalButton():
+    xPath = '//*[@id="btn_Arrivals"]'
+    arrivalButton = driver.find_element_by_xpath(xPath)
+    arrivalButton.click()    
+    
+def searchCity(cityName):
+    inputCity = driver.find_element_by_id('yyc-searchInput').clear() 
+    inputCity = driver.find_element_by_id('yyc-searchInput')   
+    inputCity.send_keys(cityName) 
+    
+def checkPath(xPath):
+    try:
+        driver.find_element_by_class_name("btn-show-earlierFlights")
+    except NoSuchElementException:
+        return False
+    return True
+    
+def clickLoadFlightsButton():  
+    xPath = '//*[@id="btn_EarlierFlights"]' 
+    result = checkPath(xPath) 
+    if(result): 
+        loadButton = driver.find_element_by_xpath(xPath) 
+        loadButton.click() 
+    time.sleep(1)
+    
+def getFlightSched(): 
+    flightInfo = []
+    dateToday = date.today()
+    
+    dateTodayFormatted = dateToday.strftime('%m/%d/%Y')
+    
+                          
+    for flightDetails  in driver.find_elements_by_class_name('yyc-flightDetails '):
+        flightNumber = flightDetails .find_element_by_xpath('.//div[@class="airlineCodeDetail"]').text #".//" the dot indicates children of the parent class
+        city = flightDetails.find_element_by_xpath('.//div[@class="airportDetail"]').text
+        flightStatus = flightDetails.find_element_by_xpath('.//div[@class="statusDetail"]').text
+        if(str(flightStatus) != ("Cancelled")):
+            flightInfo.append({'Date': dateTodayFormatted,'Flight': flightNumber, 'From': city})
+    time.sleep(1)
+    return flightInfo
+
+def closeBrowser():
+    driver.quit()
+    
